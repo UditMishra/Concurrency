3f93a8b33fae8add97e33aeb66635f954ebb4555
Udit Mishra <udmishra@cisco.com>
2019-03-20 12:41:49 +0530

    Changing the inner class name

--- src/com/udit/forkjoin/TestRecursiveAction.java
+++ src/com/udit/forkjoin/TestRecursiveAction.java
@@ -10,18 +10,18 @@ public class TestRecursiveAction {
 	public static void main(String[] args) {
 
 		int[] num = { 5, 9, 13, 43, 65, 86, 99 };
-		MyRecursiveAction action = new MyRecursiveAction(num);
+		SqaureAction action = new SqaureAction(num);
 		action.compute();
 	}
 
-	static class MyRecursiveAction extends RecursiveAction {
+	static class SqaureAction extends RecursiveAction {
 
 		private static final long serialVersionUID = 1L;
 
 		private static final int THRESHOLD_LENGTH = 1;
 		private int[] num;
 
-		public MyRecursiveAction(int[] num) {
+		public SqaureAction(int[] num) {
 			this.num = num;
 		}
 
@@ -35,12 +35,12 @@ public class TestRecursiveAction {
 			}
 		}
 
-		private List<MyRecursiveAction> createSubTasks() {
+		private List<SqaureAction> createSubTasks() {
 
 			int[] firstHalf = Arrays.copyOfRange(num, 0, num.length / 2);
 			int[] secondHalf = Arrays.copyOfRange(num, num.length / 2, num.length);
 
-			return Arrays.asList(new MyRecursiveAction(firstHalf), new MyRecursiveAction(secondHalf));
+			return Arrays.asList(new SqaureAction(firstHalf), new SqaureAction(secondHalf));
 
 		}
 
